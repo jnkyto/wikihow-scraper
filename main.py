ba299276c8facfcf26eec23aec89a53055976592
Joona Kytöniemi <jnkyto@utu.fi>
2023-06-30 10:45:32 +0300

    Fix bugs, make batching a bit different

--- main.py
+++ main.py
@@ -11,8 +11,9 @@ from article import get_article
 random_url = "https://www.wikihow.com/Special:Randomizer"
 specific_url = "https://www.wikihow.com/Refill-a-Fire-Extinguisher"
 
-# set to 1 to test with a specific url declared in specific_url and lower batches to 1
+# set to 1 to test with a specific url declared in specific_url
 USE_DEBUG = 0
+BATCH_SIZE = 100
 WORKDIR = "work"
 URL = specific_url if USE_DEBUG else random_url
 
@@ -21,22 +22,37 @@ def batch_and_dump(batches):
     if not os.path.exists(WORKDIR):
         os.makedirs(WORKDIR)
 
+    timestamp: str = datetime.now().strftime('%Y%m%d-%H%M')
     json_batch = {}
     done_hashes = []
-    for i in range(batches):
-        json_batch[f"{i}"], url, url_hash = get_article(URL)
+    for j in range(1, batches + 1):
+        print(f"Starting batch {j} of {batches}...")
+        i = 0
+        while i < BATCH_SIZE:
+            sleep(0.5)  # let's not flood requests
+            article_data = {}
 
-        if url_hash in done_hashes:
-            print(f"Hash {url_hash} already scraped! Skipping...")
-            i -= 1
-            continue
-        done_hashes.append(url_hash)
+            # handle 0.001% of articles that somehow manage to error out
+            try:
+                article_data, url, url_hash = get_article(URL)
+            except:
+                print(f"An error occurred while getting the article. Skipping...")
+                continue
 
-        print(f"Art {i + 1}/{batches} done. URL: {url}, Hash: {url_hash}")
-        sleep(0.3)  # let's not flood requests
+            if url_hash in done_hashes:
+                print(f"Hash {url_hash} already scraped during this run! Skipping...")
+                continue
 
-    with open(f"{WORKDIR}/batch_{datetime.now().strftime('%Y%m%d-%H%M%S')}.json", "w") as file:
-        json.dump(json_batch, file)
+            done_hashes.append(url_hash)
+            json_batch[f"{i}"] = article_data
+
+            print(f"Art {i + 1}/{BATCH_SIZE} done. URL: {url}, Hash: {url_hash}")
+            i += 1
+
+        with open(f"{WORKDIR}/{timestamp}_batch_{j}.json", "w") as file:
+            json.dump(json_batch, file)
+
+        print(f"Batch {j} done and written to file.")
 
 
 if __name__ == "__main__":
